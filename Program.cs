933f827a190507fd43624fc5cba8e2779b08746d
Ilia <ikudashev93@mail.ru>
2022-01-30 21:44:33 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Проверка прав администратора");
+
+[DllImport("Shell32.dll")]
+static extern bool IsUserAnAdmin();
+
+if (IsUserAnAdmin()==true) Console.WriteLine("Приложение запущено с правами админиcтратора");
+else Console.WriteLine("Приложение запущено без прав администратора");
+Console.ReadLine();
